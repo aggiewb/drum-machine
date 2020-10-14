8660edcd1672a24d9995c4c7270303f33f444b06
AggieWB <usmaggie23@gmail.com>
2020-10-14 11:14:18 -0700

    Import audio clips, and add to paths object to reference
    dynamically in audio element src path

--- client/src/components/DrumPad.js
+++ client/src/components/DrumPad.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import A from '../drum-sounds/A-drum.mp3';
+import C from '../drum-sounds/C-drum.mp3';
+import D from '../drum-sounds/D-drum.mp3';
+import E from '../drum-sounds/E-drum.mp3';
+import S from '../drum-sounds/S-drum.mp3';
+import Q from '../drum-sounds/Q-drum.mp3';
+import W from '../drum-sounds/W-drum.mp3';
+import X from '../drum-sounds/X-drum.mp3';
+import Z from '../drum-sounds/Z-drum.mp3';
+
+const paths = { A, C, D, E, S, Q, W, X, Z };
 
 function DrumPad(props){
-    return <span className="drum-pad" id={props.pad}>
-        <audio src={`${props.pad}-drum.mp3`} className="clip" id={props.pad}></audio>
+    return <span className="drum-pad" id={`${props.pad}-pad`} onClick={props.playClip}>
+        <audio src={paths[props.pad]} className="clip" id={props.pad}></audio>
         {props.pad}
     </span>;
 }
