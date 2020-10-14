b756d1b656a29c3671526e6d1b86e8a598d57554
AggieWB <usmaggie23@gmail.com>
2020-10-14 09:10:54 -0700

    Add audio element for each DrumPad with a class name of clip,
    a src attribute pointing to a audio clip, and an id corresponding to the inner text of its parent .drum-pad per user story #4

--- client/src/components/DrumPad.js
+++ client/src/components/DrumPad.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 function DrumPad(props){
     return <span className="drum-pad" id={props.pad}>
+        <audio src={`${props.pad}-drum.mp3`} className="clip" id={props.pad}></audio>
         {props.pad}
     </span>;
 }
