acd95a7f9c1bb1db19366fc6c501f4d5342ce70b
AggieWB <usmaggie23@gmail.com>
2020-10-15 09:13:54 -0700

    Move drum-pads id and its content to a new div, and add a p
    tag element with an id of display for a sound string

--- client/src/components/Drum.js
+++ client/src/components/Drum.js
@@ -2,8 +2,11 @@ import React from 'react';
 import DrumPad from './DrumPad.js'
 
 function Drum(props){
-    return <section id="display">
-        {props.pads.map(pad => <DrumPad key={pad} pad={pad} playClip={props.playClip}/>)}
+    return <section>
+        <div id="drum-pads">
+            {props.pads.map(pad => <DrumPad key={pad} pad={pad} playClip={props.playClip}/>)}
+        </div>
+        <p id="display"></p>
     </section>;
 }
 
