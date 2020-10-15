14d6259c929fc78ff5d9fa14d621cf20a4a36b1a
AggieWB <usmaggie23@gmail.com>
2020-10-15 09:16:39 -0700

    Update drumPads to be an object with values set to sound
    strings
    Add conditional in play() method to add and remove class of drum-keydown for hover effect with keydown events
    Add displaySoundString() method, and add a call of the method in play()

--- client/src/App.js
+++ client/src/App.js
@@ -7,26 +7,49 @@ class App extends React.Component {
   constructor(){
     super();
     this.play = this.play.bind(this);
+    this.displaySoundString = this.displaySoundString.bind(this);
   }
 
   componentDidMount(){
     document.addEventListener('keydown', event => this.play(document.getElementById(event.key.toUpperCase())));
   }
 
+  displaySoundString(id){
+    const display = document.getElementById('display');
+    display.textContent = this.drumPads[id];
+  }
+
   play(element){
     if(!element){
       return;
     }
+    if(!element.target){
+      const span = document.getElementById(`${element.id}-pad`);
+      span.classList.add('drum-keydown');
+      setTimeout(() => span.classList.remove('drum-keydown'), 100);
+    }
     const pad = (element.target && element.target.firstChild) || element;
+    const id = (element.target && element.target.id[0]) || element.id;
+    this.displaySoundString(id);
     pad.play();
   }
 
-  drumPads = ['Q', 'W', 'E', 'A', 'S', 'D', 'Z', 'X', 'C'];
-
+  drumPads = {
+    Q: 'bwohm', 
+    W: 'bwahm', 
+    E: 'bwaah', 
+    A: 'doeh', 
+    S: 'dooeh',
+    D: 'doooeh', 
+    Z: 'dooooeh', 
+    X: 'doooooeh', 
+    C: 'dooooooeh'
+  };
+  
   render(){
     return <main id="drum-machine">
       <h1>Drum Machine</h1>
-      <Drum pads={this.drumPads} playClip={this.play}/>
+      <Drum pads={Object.keys(this.drumPads)} playClip={this.play} />
       <Footer />
     </main>;
   }
