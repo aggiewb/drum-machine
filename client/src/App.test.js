27364b8e0c6b9d1dbbabc863b4c98acfa77918e8
AggieWB <usmaggie23@gmail.com>
2020-10-15 10:03:37 -0700

    Add test for calling playClip on click of span element in
    DrumPad

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
 import App from './App';
+import Drum from './components/Drum.js';
+import DrumPad from './components/DrumPad.js';
 
 //https://jestjs.io/docs/en/expect
 //shallow: https://enzymejs.github.io/enzyme/docs/api/shallow.html
@@ -33,4 +35,13 @@ it('should render App class child components, and initialize their props', () =>
   expect(drum.prop('pads')).toEqual(EXPECTED_DRUM_PADS_KEYS);
 
   expect(footer.exists()).toEqual(true);
+});
+
+it('should be able to call playClip on click of span element in DrumPad', () => {
+  const playClip = jest.fn();
+  const drumPad = shallow(<DrumPad playClip={playClip}/>);
+  const span = drumPad.find('span');
+
+  span.simulate('click');
+  expect(playClip).toHaveBeenCalled();
 });
