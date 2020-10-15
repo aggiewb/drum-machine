ff547377bed3f05dbb1388ffd4621fac53bfc905
AggieWB <usmaggie23@gmail.com>
2020-10-15 09:44:58 -0700

    Add test for rendering App class child components, and
     initialize their props
    Add constants EXPECTED_DRUM_PADS and EXPECTED_DRUM_PADS_KEYS

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -6,7 +6,31 @@ import App from './App';
 //shallow: https://enzymejs.github.io/enzyme/docs/api/shallow.html
 //mount: https://enzymejs.github.io/enzyme/docs/api/mount.html
 
+const EXPECTED_DRUM_PADS = {
+  Q: 'bwohm', 
+  W: 'bwahm', 
+  E: 'bwaah', 
+  A: 'gong', 
+  S: 'goong',
+  D: 'gooong', 
+  Z: 'goooong', 
+  X: 'gooooong', 
+  C: 'goooooong'
+};
+const EXPECTED_DRUM_PADS_KEYS = Object.keys(EXPECTED_DRUM_PADS);
 
 it('App deeply renders as a smoke test', () => {
   mount(<App />);
+});
+
+it('should render App class child components, and initialize their props', () => {
+  const app = shallow(<App />);
+  const drum = app.find('Drum');
+  const footer = app.find('Footer');
+
+  expect(drum.exists()).toEqual(true);
+  expect(drum.prop('playClip')).toBeDefined();
+  expect(drum.prop('pads')).toEqual(EXPECTED_DRUM_PADS_KEYS);
+
+  expect(footer.exists()).toEqual(true);
 });
